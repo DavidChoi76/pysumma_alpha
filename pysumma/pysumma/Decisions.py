371daf4c829a15bffbf0bb948dba97abad5c5552
Youngdon CHOI <choiyd1115@gmail.com>
2017-12-22 18:13:41 -0500

    Update Decisions.py

--- pysumma/pysumma/Decisions.py
+++ pysumma/pysumma/Decisions.py
@@ -1,9 +1,13 @@
 class Decisions:
 	def __init__(self, path):
 		self.path = path
-		
+		#load into memory the decisions file
+
 	def SetStomatalResistanceFunction(self, name):
 		#update decision file 
 		pass
-	
+
+	def SavetoDisk(self):
+		#save decisions file to disk
+		pass
 	
