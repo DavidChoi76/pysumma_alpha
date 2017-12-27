f9d261ec2f1d01e7d2e66f0ad37569f10fbca606
YOUNGDON CHOI <yc5ef@virginia.edu>
2017-12-26 23:01:26 -0500

    Added unit test

--- pysumma/setup.py
+++ pysumma/setup.py
@@ -1,13 +1,33 @@
-from setuptools import setup
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 setup(name='pysumma',
       version = '0.1',
       description = 'This is pySUMMA',
-      url = '',
-      author = 'Choi',
+      url = 'https://github.com/DavidChoi76/pysumma_alpha',
+      author = 'YoungDon Choi',
       author_email = 'yc5ef@virginia.edu',
       license = 'MIT',
       packages=['pysumma'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      cmdclass = {'test': PyTest})
+      #...,)
+
+#      zip_safe=False)
