a191fa7580ad1df0a557ddae9eba596a4de3d81a
YOUNGDON CHOI <yc5ef@virginia.edu>
2018-01-09 13:19:35 -0500

    add a few test statement

--- pysumma/pysumma/scratch.py
+++ pysumma/pysumma/scratch.py
@@ -1,26 +1,24 @@
 from pysumma.pysumma.Decisions import Decisions
 
-D_file = Decisions('D:\\pysumma\\pysumma_alpha\\pysumma\\pysumma\\', 'Decision2.txt')
+D_file = Decisions('D:\\pysumma\\pysumma_alpha\\pysumma\\pysumma\\', 'Decision.txt')
 
 soil_cat_dataset = D_file.soilCatTbl
 
-print(soil_cat_dataset.name)
-print(soil_cat_dataset.value)
-print(soil_cat_dataset.options)
-print(soil_cat_dataset.description)
-# print(soil_cat_dataset.date_time)
+# print(soil_cat_dataset.name)
+# print(soil_cat_dataset.value)
+# print(soil_cat_dataset.option)
+# print(soil_cat_dataset.description)
+#
+# print(D_file.bcLowrTdyn.name)
+# print(D_file.bcLowrTdyn.value)
+# print(D_file.bcLowrTdyn.option)
+# print(D_file.bcLowrTdyn.description)
 
-D_file.bcLowrTdyn.value = 'choi'
-soil_cat_dataset.value = "acaddxaas"
-D_file.num_method.value = 'jeff'
-D_file.LAI_method.value = 'moh'
+soil_cat_dataset.value = "STAS1"
+# D_file.num_method.value = 'non-iter'
+# D_file.bcLowrTdyn.value = "presTemp"
+# D_file.bcLowrSoiH.value = 'presHead'
 
-print(soil_cat_dataset.value)
 
 sim_start_time = D_file.simulStart
-#
-print(sim_start_time.value)
-#
-# sim_start_time.value = "2013-11-15 11:04"
-#
-# print(sim_start_time.value)
+sim_start_time.value = "2013-11-15 11:04"
