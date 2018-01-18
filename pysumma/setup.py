2aff4472164d8e336b0030ea8486da2edb75e0bc
YOUNGDON CHOI <yc5ef@virginia.edu>
2018-01-17 20:34:58 -0500

    update setup.py to do a unit test using unittest library

--- pysumma/setup.py
+++ pysumma/setup.py
@@ -1,21 +1,4 @@
-import sys
-
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ''
-
-    def run_tests(self):
-        import shlex
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 setup(name='pysumma',
       version = '0.1',
@@ -25,9 +8,6 @@ setup(name='pysumma',
       author_email = 'yc5ef@virginia.edu',
       license = 'MIT',
       packages=['pysumma'],
-      setup_requires=['pytest-runner', 'xarray'],
-      tests_require=['pytest'],
-      cmdclass = {'test': PyTest})
-      #...,)
-
-#      zip_safe=False)
+      test_suite = 'tests')
+#      setup_requires=['pytest-runner', 'xarray'],
+#      test_suite='pysumma.tests.Decisions_suite')
